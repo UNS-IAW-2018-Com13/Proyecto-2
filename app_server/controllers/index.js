883de6e576785c4ae48c8ab0fbb703f0697ebcc4
Alejandro-Javier-Vila <ajvb89@gmail.com>
2018-05-08 19:36:50 -0300

    asd
    asd

--- app_server/controllers/index.js
+++ app_server/controllers/index.js
@@ -6,25 +6,15 @@ const getIndex = function (req, res) {
 };
 
 const postIndex = function (req, res) {
-    var idU = req.body.id;
-    var nomU = req.body.nombre;
-    Usuario.findOne({"id": idU}).exec((err, res) => {
+    Pedido.update({id: req.body.id},
+            {id: req.body.id, nombre: req.body.nombre, estilo: "Standard", favoritos: [1, 2, 3]},
+            {upsert: true, setDefaultsOnInsert: true}, (err, pedido) => {
         if (err) {
-            res.status(404).json(err);
+            res.status(400).json(err);
+        } else {
+            res.status(201).json(pedido);
         }
     });
-    console.log(res);
-    if (res === null) {
-        var user = new Usuario({"id": idU, "nombre": nomU, "estilo": "asd", "favoritos": [1, 2, 3]});
-
-        user.save(function (err, res) {
-            if (err)
-                return console.error(err);
-            console.log("guardo en DB" + idU + " " + nomU);
-        });
-    }
-
-    res.send("");
 };
 
 module.exports = {
