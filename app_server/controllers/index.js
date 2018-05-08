1823336ac3c270601c67fa0e397e1e0d4a6aa1b5
Alejandro-Javier-Vila <ajvb89@gmail.com>
2018-05-08 17:55:56 -0300

    asd
    asd

--- app_server/controllers/index.js
+++ app_server/controllers/index.js
@@ -1,24 +1,32 @@
 const mongoose = require('mongoose');
 const Usuario = mongoose.model('Usuario');
 
-const getIndex = function(req, res) {
-  res.render('index');
+const getIndex = function (req, res) {
+    res.render('index');
 };
 
-const postIndex = function(req, res) {
-  var idU = req.body.id;
-  var nomU = req.body.nombre;
-  
-  var user = new Usuario({ "id": idU, "nombre": nomU, "estilo": "asd" , "favoritos":[1,2,3]});
- 
-  user.save(function (err, res) {
-    if (err) return console.error(err);
-    console.log("guardo en DB" + idU + " " + nomU);
-  });
-  
-  res.send("");
+const postIndex = function (req, res) {
+    var idU = req.body.id;
+    var nomU = req.body.nombre;
+    Usuario.findOne({"id": idU}).exec((err, res) => {
+        if (err) {
+            res.status(404).json(err);
+        }
+    });
+
+    if (res === null) {
+        var user = new Usuario({"id": idU, "nombre": nomU, "estilo": "asd", "favoritos": [1, 2, 3]});
+
+        user.save(function (err, res) {
+            if (err)
+                return console.error(err);
+            console.log("guardo en DB" + idU + " " + nomU);
+        });
+    }
+
+    res.send("");
 };
 
 module.exports = {
-  getIndex, postIndex
+    getIndex, postIndex
 };
