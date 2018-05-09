6ea525bea2224dfc72b93c384ed326eb38d7990a
Alejandro-Javier-Vila <ajvb89@gmail.com>
2018-05-08 23:57:50 -0300

    asd
    asd

--- app_server/controllers/index.js
+++ app_server/controllers/index.js
@@ -6,12 +6,12 @@ const getIndex = function (req, res) {
 };
 
 const postIndex = function (req, res) {
-    Usuario.update({idUser: req.body.idUser},
+    Usuario.update({idUser: req.body.idUser}, {idUser: req.body.idUser, estilo: "Standard", favoritos: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
             {upsert: true, setDefaultsOnInsert: true}, (err, resultado) => {
         if (err) {
             res.status(400).json(err);
         } else {
-            res.status(200).json(resultado);
+            res.status(201).json(resultado);
         }
     });
 };
