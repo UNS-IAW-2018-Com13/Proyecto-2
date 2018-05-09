b51e6910d7b9ac22a8ba166a97e1c51e6188bccd
Alejandro-Javier-Vila <ajvb89@gmail.com>
2018-05-09 18:42:35 -0300

    asd
    asd

--- app_server/controllers/index.js
+++ app_server/controllers/index.js
@@ -6,14 +6,26 @@ const getIndex = function (req, res) {
 };
 
 const postIndex = function (req, res) {
-    Usuario.update({idUser: req.body.idUser}, {idUser: req.body.idUser, estilo: "Standard", favoritos: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},
-            {upsert: true, setDefaultsOnInsert: true}, (err, resultado) => {
+
+    /*
+     Usuario.update({idUser: req.body.idUser}, {idUser: req.body.idUser, estilo: "Standard", favoritos: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]},
+     {upsert: true, setDefaultsOnInsert: true}, (err, resultado) => {
+     if (err) {
+     res.status(400).json(err);
+     } else {
+     res.status(201).json(resultado);
+     }
+     });
+     */
+    Usuario.findOne({idUser: req.body.idUser}, function (err, usr) {
         if (err) {
-            res.status(400).json(err);
+            usr.idUser = req.body.idUser;
+            usr.save();
         } else {
-            res.status(201).json(resultado);
+            res.send(usr);
         }
     });
+
 };
 
 module.exports = {
