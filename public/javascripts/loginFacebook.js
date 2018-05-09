533bc1236f209f7836005f0d4444d2c82764dd80
Alejandro-Javier-Vila <ajvb89@gmail.com>
2018-05-08 23:32:48 -0300

    asd
    asd

--- public/javascripts/loginFacebook.js
+++ public/javascripts/loginFacebook.js
@@ -38,9 +38,9 @@ window.fbAsyncInit = function () {
 function cargarDatosUsuario(){
     FB.api('/me', function (response) {
         $.post('/', {"idUser": response.id}, function(req, res){
-            var estilo = res.body.estilo;
-            var favoritos = res.body.favoritos;
-            guardarDatos(estilo, favoritos);
+            //var estilo = res.body.estilo;
+            //var favoritos = res.body.favoritos;
+            //guardarDatos(estilo, favoritos);
         });
     });
 }
