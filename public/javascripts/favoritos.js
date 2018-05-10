25038068c77515a26fd6e2d32d685bb6c6fc06dd
Alejandro-Javier-Vila <ajvb89@gmail.com>
2018-05-10 14:05:28 -0300

    asd
    asd

--- public/javascripts/favoritos.js
+++ public/javascripts/favoritos.js
@@ -1,19 +1,29 @@
+function controlarLogin(jnom) {
+    FB.getLoginStatus(function (response) {
+        if (response.status === 'connected') {
+            setearFavoritos(jnom);
+        } else{
+            FB.login();
+        }
+    });
+}
+
 function setearFavorito(jnom) {
     var estilo = obtenerEstilo();
     var fav = obtenerFavorito(jnom);
     if (fav === "no") {
-        guardarFavorito(jnom,"si");
+        guardarFavorito(jnom, "si");
         if (estilo === "Standard") {
             document.getElementById(jnom).setAttribute("src", "/images/icons/fav_std.png");
         } else {
             document.getElementById(jnom).setAttribute("src", "/images/icons/fav_nax.png");
         }
     } else {
-        guardarFavorito(jnom,"no");
+        guardarFavorito(jnom, "no");
         //if (estilo === "Standard") {
-            document.getElementById(jnom).setAttribute("src", "/images/icons/no_fav_std.png");
+        document.getElementById(jnom).setAttribute("src", "/images/icons/no_fav_std.png");
         //} else {
-            //document.getElementById(jnom).setAttribute("src", "/images/icons/no_fav_nax.png");
+        //document.getElementById(jnom).setAttribute("src", "/images/icons/no_fav_nax.png");
         //}
     }
 }
