26207d8a4010de84eed4f315abdce20041ab01ca
Raptor <ruben_dario2001ar@hotmail.com>
2018-05-23 14:46:33 -0300

    Revert "Merge branch 'Beta' of https://github.com/UNS-IAW-2018-Com13/Proyecto-2 into Beta"
    This reverts commit e9ad0dade232281b2f09200ea56c2789db963d9a, reversing
    changes made to 01ea1c88bbe1fb860c1f5f42a2dbf777c5041add.

--- public/javascripts/buscador.js
+++ public/javascripts/buscador.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
 function buscar(criterio){    
     $.post("/buscador/g_carta", {'crit': document.getElementById(criterio).value});
-=======
-function buscarCarta() {
-    $.post('/buscador/g_carta', {'carta': document.getElementById("texto").value}, function (res, req) {
-        console.log("RES: " + res.respuesta);
-        console.log("REQ: " + req);
-    });
->>>>>>> 941d2dea68720de64e246ebdbdac8838cc161a00
 }
