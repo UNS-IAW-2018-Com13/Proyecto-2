94725223095e09cbcc6a79a3b322e570c80da201
Alejandro-Javier-Vila <ajvb89@gmail.com>
2018-06-02 02:55:25 -0300

    asd
    asd

--- app_server/controllers/index.js
+++ app_server/controllers/index.js
@@ -1,6 +1,15 @@
+const mongoose = require('mongoose');
+const Usuario = mongoose.model('Usuario');
+
 const getIndex = function (req, res) {
     if(req.user){
-       res.render('index',{usuario: req.user}); 
+       Usuario.findOne({'id': req.user.id}, (err, resultado) => {
+            if (err) {
+                res.status(400).json(err);
+            } else {
+                res.render('index',{usuario: req.user});
+            }
+        });
     }else{
         res.render('index');
     }
