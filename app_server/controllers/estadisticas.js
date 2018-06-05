00f8b0bd63cb1f9da1543b8e484b72c01b4f4ddd
Alejandro-Javier-Vila <ajvb89@gmail.com>
2018-06-05 03:00:48 -0300

    asd
    asd

--- app_server/controllers/estadisticas.js
+++ app_server/controllers/estadisticas.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const Jugador = mongoose.model('Jugador');
-
 const Usuario = mongoose.model('Usuario');
 
 const getEstadisticas = function (req, res) {
@@ -14,10 +13,20 @@ const getEstadisticas = function (req, res) {
                 player[i] = jugadores[i].nombre;
                 votes[i] = jugadores[i].favorito;
             }
-            favoritos =new Object();
-            favoritos.jugadores= player;
-            favoritos.votos= votes;
-            res.render('estadisticas',{favoritos});
+            favoritos = new Object();
+            favoritos.jugadores = player;
+            favoritos.votos = votes;
+            if (req.user) {
+                Usuario.findOne({'id': req.user.id}, (err, resultado) => {
+                    if (err) {
+                        res.status(400).json(err);
+                    } else {
+                        res.render('estadisticas', {usuario: resultado, favoritos});
+                    }
+                });
+            } else {
+                res.render('estadisticas', {favoritos});
+            }
         }
     });
 };
