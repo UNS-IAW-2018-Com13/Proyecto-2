fd2ee9ef4a6de5f91bcf78c7d25b4a923146ae5d
Alejandro-Javier-Vila <ajvb89@gmail.com>
2018-06-13 05:25:52 -0300

    asd
    asd

--- app_server/models/partidos.js
+++ app_server/models/partidos.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const partidoSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true
+  },
   fecha: {
     type: String,
     required: true
@@ -13,23 +17,18 @@ const partidoSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  mazosJugador1: {
-    type: [Number],
-    required: true
-  },
   jugador2: {
     type: String,
     required: true
   },
-  mazosJugador2: {
-    type: [Number],
-    required: true
-  },
-  ganador: {
-    type: [Number],
-    required: true
+  rounds: {
+      type: [{ganador: {type: String}, mazoG: {type: String}, mazoP: {type: String} }]
   },
   editor: {
+    type: String,
+    required: true
+  },
+  comentario: {
     type: String,
     required: true
   }
